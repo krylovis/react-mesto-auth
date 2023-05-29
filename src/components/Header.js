a9df236876ea3ba88c09a4431c961a4a85a7a351
Krylov Ivan <krylovis@mchs.ru>
2023-05-29 12:35:47 +0300

    fix Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header(props) {
   const { userEmail, handleSetLoggedOut, loggedIn } = props;
   const [isOpen, setIsOpen] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
   const navigate = useNavigate();
-
-  const location = useLocation();
-  const { pathname } = location;
 
   const onSignOut = () => {
     localStorage.removeItem('mesto-react-token');
@@ -18,25 +16,36 @@ export default function Header(props) {
   };
 
   React.useEffect(() => {
-    const resize = () => document.body.clientWidth > 420 && setIsOpen(false);;
+    const resize = () => {
+      const mobileWidth = document.body.clientWidth < 420;
+      if (mobileWidth) {
+        setIsMobile(true);
+      } else {
+        setIsOpen(false);
+        setIsMobile(false);
+      }
+    };
     resize();
     window.addEventListener('resize', resize);
     return () => window.removeEventListener("resize", resize);
-  });
+  }, [setIsMobile]);
 
   const toggleMenu = () => setIsOpen(isOpen => !isOpen);
 
   const menuButtonClassName = `button header__menu-button ${isOpen ? 'header__menu-button_type_closed' : ''}`;
   const linksContainerClassName = `header__links-container ${isOpen ? 'header__links-container_type_closed' : ''}`;
 
+  const navForLoggedIn = () =>
+    <nav className={linksContainerClassName}>
+      <span className="header__login" title={userEmail}>{userEmail}</span>
+      <button className="button header__out-button" onClick={onSignOut}>Выйти</button>
+    </nav>;
+
   return (
     <header className="header">
       {(isOpen && loggedIn) &&
         <div className='header__login-container'>
-          <nav className={linksContainerClassName}>
-            {pathname === "/" && <span className="header__login" title={userEmail}>{userEmail}</span>}
-            {pathname === "/" && <button className="button header__out-button" onClick={onSignOut}>Выйти</button>}
-          </nav>
+          {navForLoggedIn()}
         </div>
       }
 
@@ -45,16 +54,13 @@ export default function Header(props) {
           <img src={logo} alt="Логотип: Место" className="header__logo" />
         </NavLink>
 
-        {(!isOpen && !loggedIn) &&
-          <nav className={linksContainerClassName}>
-            {pathname === "/sign-up" && <NavLink className="link header__link" to="/sign-in">Войти</NavLink>}
-            {pathname === "/sign-in" && <NavLink className="link header__link" to="/sign-up">Регистрация</NavLink>}
-            {pathname === "/" && <span className="header__login" title={userEmail}>{userEmail}</span>}
-            {pathname === "/" && <button className="button header__out-button" onClick={onSignOut}>Выйти</button>}
-          </nav>
-        }
+        <Routes>
+          <Route path="/" element={(loggedIn && !isMobile) && navForLoggedIn()} />
+          <Route path="/sign-up" element={<NavLink className="link header__link" to="/sign-in">Войти</NavLink>} />
+          <Route path="/sign-in" element={<NavLink className="link header__link" to="/sign-up">Регистрация</NavLink>} />
+        </Routes>
 
-        {loggedIn && <button
+        {(loggedIn && isMobile) && <button
           className={menuButtonClassName}
           type="button"
           title={isOpen ? 'Закрыть меню' : 'Открыть меню'}
